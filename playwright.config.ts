84e07176b59304f02c6a0607de9a6a167efb46fe
GitStashes <nikola.stasic@clickoutmedia.com>
2025-03-05 21:57:25 +0100

    Adding video in report

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    //Add video recording
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
